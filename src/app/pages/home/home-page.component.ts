24e8672f6e88fe6e209df9241dc49d49477dc81d
Vincent MIJOULE <vincent.mijoule@izichange.fr>
2026-03-18 14:15:42 +0100

    Add video

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  @ViewChild('heroVideo')
+  private readonly heroVideo?: ElementRef<HTMLVideoElement>;
+
+  protected isMuted = true;
+
+  protected toggleMuted(): void {
+    const video = this.heroVideo?.nativeElement;
+    if (!video) {
+      this.isMuted = !this.isMuted;
+      return;
+    }
+
+    const nextMuted = !this.isMuted;
+    video.muted = nextMuted;
+    video.defaultMuted = nextMuted;
+
+    if (!nextMuted) {
+      video.volume = 1;
+      void video.play();
+    }
+
+    this.isMuted = nextMuted;
+  }
+}
--- src/app/pages/support/support-page.component.ts
+++ src/app/pages/support/support-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-support-page',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './support-page.component.html',
   styleUrl: './support-page.component.css'
 })
-export class SupportPageComponent {}
+export class SupportPageComponent {
+  @ViewChild('donationVideo')
+  private readonly donationVideo?: ElementRef<HTMLVideoElement>;
+
+  protected isMuted = true;
+
+  protected toggleMuted(): void {
+    const video = this.donationVideo?.nativeElement;
+    if (!video) {
+      this.isMuted = !this.isMuted;
+      return;
+    }
+
+    const nextMuted = !this.isMuted;
+    video.muted = nextMuted;
+    video.defaultMuted = nextMuted;
+
+    if (!nextMuted) {
+      video.volume = 1;
+      void video.play();
+    }
+
+    this.isMuted = nextMuted;
+  }
+}
